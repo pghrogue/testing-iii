9e3a4e6ab367045ba36d1a5eabf2e5817132a27a
Jennifer King <jenbellpgh@gmail.com>
2019-02-05 22:44:13 -0500

    Started.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
 // Test away
+
+
+
+// displays 'Closed' if the closed prop is true and 'Open' if otherwise
+// displays 'Locked' if the locked prop is true and 'Unlocked' if othewise
+// when locked or closed use the red-led class
+// when unlocked or open use the green-led class
