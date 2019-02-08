61df8d97de682831316b37a8beed483d6f42265b
Jennifer King <jenbellpgh@gmail.com>
2019-02-08 00:53:12 -0500

    Finally got this started. Writing Display.spec.js tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,44 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from './Controls';
+
+describe('Controls functions', () => {
+
+  it('executes the function when lock is clicked', () => {
+    const mockFun = jest.fn();
+    const { getByText } = render(<Controls toggleLocked={mockFun} closed={true} locked={false}/>);
+    const button = getByText(/lock gate/i);
+
+    fireEvent.click(button);
+
+    expect(mockFun).toHaveBeenCalledTimes(1);
+  });
+
+
+  it('executes the function when close is clicked', () => {
+    const mockFun = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={mockFun} closed={false}/>);
+    const button = getByText(/close gate/i);
+
+    fireEvent.click(button);
+
+    expect(mockFun).toHaveBeenCalledTimes(1);
+  });
+
+  
+  it('disables the locked button if the gate is open', () => {
+    const mockFun = jest.fn();
+    const { getByText } = render(<Controls toggleLocked={mockFun} closed={false} />);
+    const button = getByText(/lock gate/i);
+
+    expect(button).toBeDisabled();
+
+    fireEvent.click(button);
+
+    expect(mockFun).toHaveBeenCalledTimes(0);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display';
+
+describe('Display component', () => {
+  it('should show Unlocked when unlocked', () => {
+    const { getByText } = render(<Display locked={false}/>);
+    const output = getByText(/unlocked/i);
+
+    expect(output).toBeDefined();
+  });
+
+  it('show Locked when locked', () => {
+    const { getByText } = render(<Display locked={true} />);
+    const output = getByText(/locked/i);
+
+    expect(output).toBeDefined();
+  });
+});
